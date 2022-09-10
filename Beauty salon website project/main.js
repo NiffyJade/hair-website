a8511945b701fa1a4945c96c6ee2614876962e45
NiffyJade <adefisayooluwanifemi@yahoo.com>
2022-09-10 14:35:49 +0100

    Add files via upload

---
+++ Beauty salon website project/main.js
@@ -0,0 +1,39 @@
+//show navbar
+
+const nav = document.querySelector('.nav-menu');
+const toggle = document.querySelector('.nav-toggle');
+toggle.onclick = function() {
+        nav.classList.toggle('show-nav')
+    }
+    // Remove navbar
+
+const navLink = document.querySelectorAll('.nav-link')
+
+function listAction() {
+    const navMenu = document.querySelector('.nav-menu')
+    navMenu.classList.remove('show-nav')
+}
+navLink.forEach(n => n.addEventListener('click', listAction))
+
+// Change Active color
+const linkColor = document.querySelectorAll('.nav-link')
+
+function colorLink() {
+    if (linkColor) {
+        linkColor.forEach(L => L.classList.remove('active'))
+        this.classList.add('active')
+    }
+}
+linkColor.forEach(L => L.addEventListener('click', colorLink))
+
+// Change Header Background when scroll down
+
+function scrollHeader() {
+    const scrollHeader = document.getElementById('header')
+    if (this.scrollY >= 200) {
+        scrollHeader.classList.add('scroll-header')
+    } else {
+        scrollHeader.classList.remove('scroll-header')
+    }
+}
+window.addEventListener('scroll', scrollHeader);
